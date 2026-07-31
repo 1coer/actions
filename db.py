abc7a905bee61ed475a0db8cbeaa17a74409dcaa
hireki <hireki@proton.me>
2026-07-31 13:45:22 -0300

    get .env vars through python-dotenv

--- db.py
+++ db.py
@@ -1,22 +1,18 @@
+import os
+
 from sqlmodel import SQLModel, create_engine
 
+from dotenv import load_dotenv
+
+# Load the variables from the .env file into the system environment
+load_dotenv()
 
 # sqlite_file_name = "database.db"
 # sqlite_url = f"sqlite:///{sqlite_file_name}"
 # connect_args = {"check_same_thread": False}
 # engine = create_engine(sqlite_url, connect_args=connect_args)
 
-# pip install python-dotenv
-# from dotenv import load_dotenv
-#
-# # Load the variables from the .env file into the system environment
-# load_dotenv()
-#
-# # Access the variables using os.getenv
-# database_user = os.getenv("DB_USER")
-# api_key = os.getenv("API_KEY")
-
-database_url = "postgresql://user:password@db/dbname"
+database_url = os.getenv("DB_URL", "")
 engine = create_engine(database_url, echo=True)
 
 def create_db_and_tables():
